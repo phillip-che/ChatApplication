6c4db0071744243bb4485d972729c41570088120
phillip-che <pche@cpp.edu>
2023-11-03 00:24:55 -0700

    implement default structure of server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,25 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-console.log("HELLO WORLD");
+const port = process.env.PORT || 4000;
+const corsOrigin = "http://localhost:3000";
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: corsOrigin,
+        credentials: true,
+    }
+});
+
+app.get('/', (_, res) => {
+    res.send(`Server is up and running.`);
+});
+
+httpServer.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});

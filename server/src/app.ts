67b6b2d12fd8338972a72a091c4038458c605943
phillip-che <pche@cpp.edu>
2023-11-05 21:24:13 -0800

    establish client connection to server side

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createServer } from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 const port = process.env.PORT || 4000;
 const corsOrigin = "http://localhost:3000";
@@ -22,4 +22,12 @@ app.get('/', (_, res) => {
 
 httpServer.listen(port, () => {
     console.log(`Listening on port ${port}`);
+
+    io.on("connection", (socket: Socket) => {
+        console.log(`User ${socket.id} connected.`);
+
+        socket.on("disconnect", () => {
+            console.log(`User ${socket.id} disconncted.`);
+        });
+    });
 });

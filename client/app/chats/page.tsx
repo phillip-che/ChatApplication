4f0e4b630514f1ed24ea7240345c6ba02ad6f73f
Marvin S <mpsevilla@cpp.edu>
2023-11-09 00:11:30 -0800

    Moved code from page.tsx to Chatroom.tsx for better organization and access of elements based on the current heirarchy of the code.
    May need to change, not sure where the chatroom code I've designed fits
    better.

--- client/app/chats/page.tsx
+++ client/app/chats/page.tsx
@@ -8,43 +8,11 @@ import { useSocket } from "@/context/socket.context";
 const Page = () => {
   const { socket, username } = useSocket();
 
-  {
-    /* Formatter for creating new user-message pairs */
-  }
-  function newUsrMsg_Pair(username: string, message: string) {
-    const pair = "[" + username + "] " + message;
-    return pair;
-  }
-
-  {
-    /* Method for adding new users
-    New users should be appended to the head of the list for all users */
-  }
-  function addToMsgBuffer(username: string, message: string) {
-    chatBuffer.push(newUsrMsg_Pair(username, message));
-  }
-
-  {
-    /* 1D array that holds all chat messages */
-  }
-  let chatBuffer = [newUsrMsg_Pair("SYSTEM", "Chat service started.")];
-
-  addToMsgBuffer("TestUser", "Test Message");
-
   return (
     <main>
       <h1>Chat Room</h1>
       <h3>Username: {username}</h3>
       <ChatRoom socket="socket" username="username" />
-      <Button>
-        <Link href="/">Quit Chatroom</Link>
-      </Button>
-
-      <ul className="list-group">
-        {chatBuffer.map((element, index) => (
-          <li>{element}</li>
-        ))}
-      </ul>
     </main>
   );
 };
--- client/components/ChatRoom.tsx
+++ client/components/ChatRoom.tsx
@@ -1,6 +1,7 @@
 "use client";
-
+import Link from "next/link";
 import { Button, TextField } from "@mui/material";
+import { makeStyles } from "@mui/material";
 
 const ChatRoom = ({
   socket,
@@ -11,6 +12,29 @@ const ChatRoom = ({
   username: string;
   room?: string;
 }) => {
+  {
+    /* Formatter for creating new user-message pairs */
+  }
+  function newUsrMsg_Pair(username: string, message: string) {
+    const pair = "[" + username + "] " + message;
+    return pair;
+  }
+
+  {
+    /* Method for adding new users
+    New users should be appended to the head of the list for all users */
+  }
+  function addToMsgBuffer(username: string, message: string) {
+    chatBuffer.push(newUsrMsg_Pair(username, message));
+  }
+
+  {
+    /* 1D array that holds all chat messages */
+  }
+  let chatBuffer = [newUsrMsg_Pair("SYSTEM", "Chat service started.")];
+
+  addToMsgBuffer("TestUser", "Test Message");
+
   return (
     <>
       <TextField
@@ -19,6 +43,15 @@ const ChatRoom = ({
         fullWidth
       />
       <Button>Send</Button>
+      <Button>
+        <Link href="/">Quit Chatroom</Link>
+      </Button>
+
+      <ul className="list-group">
+        {chatBuffer.map((element, index) => (
+          <li>{element}</li>
+        ))}
+      </ul>
     </>
   );
 };
